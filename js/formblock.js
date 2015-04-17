3994254153e03a39f68cda6e300fb309f873d72c
Nathan Haug <nate@lullabot.com>
2015-04-17 01:43:52 -0700

    Issue #3: Replace Drupal with Backdrop in JS file.

--- js/formblock.js
+++ js/formblock.js
@@ -1,12 +1,12 @@
 (function ($) {
 
-Drupal.behaviors.formBlockSummaries = {
+Backdrop.behaviors.formBlockSummaries = {
   attach: function (context) {
     // Provide the summary for the node type form.
-    $('fieldset.formblock-node-type-settings-form', context).drupalSetSummary(function(context) {
+    $('fieldset.formblock-node-type-settings-form', context).backdropSetSummary(function(context) {
       var vals = [];
       $('input:checked', context).next('label').each(function() {
-        vals.push(Drupal.checkPlain($(this).text()));
+        vals.push(Backdrop.checkPlain($(this).text()));
       });
       return vals.join(', ');
     });
